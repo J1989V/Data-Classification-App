1d5ad85c5a8e1730fae618f509ee55927d7607d4
Rudi van Tonder <rudi.vantonder@re-leased.com>
2020-11-19 08:53:12 +1300

    restored old user and password things

--- routers/classifyapi.js
+++ routers/classifyapi.js
@@ -159,8 +159,8 @@ router.get('/view', (req, res) => {
             host: 'popi-db-do-user-8330663-0.b.db.ondigitalocean.com',
             port: 25060,
             database: 'defaultdb',
-            user: 'doadmin',
-            password: 'zadszp92tgcfnkmd'
+            user: process.env.USERNAME,
+            password: process.env.PASSWORD
         });
         connection.connect(function (e) {
             if (e) {
