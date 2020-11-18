41d96a7ab26cf9366f790e24efbae0fb8306b59f
Rudi van Tonder <rudi.vantonder@re-leased.com>
2020-11-19 08:45:17 +1300

    testing

--- app.js
+++ app.js
@@ -38,8 +38,8 @@ app.post('/login', function (req, res) {
       host: 'popi-db-do-user-8330663-0.b.db.ondigitalocean.com',
       port: 25060,
       database: 'defaultdb',
-      user: process.env.USERNAME,
-      password: process.env.PASSWORD
+      user: 'doadmin',
+      password: 'zadszp92tgcfnkmd'
     });
     connection.connect(function (e) {
       if (e) {
